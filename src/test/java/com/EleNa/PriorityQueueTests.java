722cf2df6c0ab5d9edfa4ee3b268f4d756507733
Nathan Scheele <nathanscheele@gmail.com>
2020-04-12 19:59:23 -0400

    AStarRouteFinder implementation. Needs to be tested, will do soon.

---
+++ src/test/java/com/EleNa/PriorityQueueTests.java
@@ -0,0 +1,22 @@
+package com.EleNa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PriorityQueueTests {
+
+
+    @BeforeEach
+    void startUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
