fffe21be08e51ed67fdc42c6306518dfd05a3e36
arthurfeeney <afeeney@umass.edu>
2020-04-15 20:32:09 -0500

    Added DijkstraRouteFinder that implements Dijkstras algorithm for shortest path route finding. Added tests that check it finds the shortest weightd path.

---
+++ src/main/java/com/EleNa/routing/DijkstraRouteFinder.java
@@ -0,0 +1,138 @@
+package com.EleNa.routing;
+
+import com.EleNa.graph.Graph;
+import com.EleNa.graph.GraphNode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Route finding algorithm that uses Dijkstra's to find the shortest path between a source and a sink node in
+ * a graph.
+ */
+public class DijkstraRouteFinder implements RouteFinder {
+    protected Graph graph;
+    protected PriorityQueue pQueue;
+
+    public DijkstraRouteFinder(Graph graph) throws IllegalArgumentException {
+        if (graph == null) {
+            throw new IllegalArgumentException("ERROR: graph must be a non-null");
+        }
+        this.graph = graph;
+        this.pQueue = new PriorityQueue(new MinPriorityComparator());
+    }
+
+    /**
+     * Initializes the priority queue so that all distances are set to infinity, expect the source node, which has its
+     * distance initialized to zero.
+     * @param source The source node that the search starts at.
+     * @return A reference to the priority queue.
+     */
+    private PriorityQueue initializeDistancesToInfinity(GraphNode source) {
+        int numNodes = this.graph.getNumNodes();
+        for(int id = 0; id < numNodes; ++id) {
+            if (id != source.getId()) {
+                PriorityQueueItem item = new PriorityQueueItem(this.graph.getNodeById(id));
+                this.pQueue.add(item);
+            }
+        }
+        PriorityQueueItem sourceItem = new PriorityQueueItem(source, 0.0, 0.0);
+        this.pQueue.add(sourceItem);
+        return this.pQueue;
+    }
+
+    /**
+     * Construct a route from the provided start and end nodes and the "previous" nodes. It starts from the sink node
+     * and works its way backwords
+     * @param source The start of the route
+     * @param sink The end of the route
+     * @param prevNode The ids of nodes along the shortest path.
+     * @return
+     */
+    private
+    Route routeFromIds(GraphNode source, GraphNode sink, HashMap<GraphNode, GraphNode> prevNode) throws IllegalArgumentException {
+        if(prevNode.get(source) != source) {
+            throw new IllegalArgumentException("Error: routeFromIds expects the prev of source to be source.");
+        }
+        Route route = new Route();
+        GraphNode currentNode = sink;
+        while(prevNode.get(currentNode) != currentNode) {
+            route.prependNode(currentNode);
+            currentNode = prevNode.get(currentNode);
+        }
+        route.prependNode(source);
+        return route;
+    }
+
+    /**
+     * Find the exact shortest path between source and sink.
+     * @param source The starting node
+     * @param sink The target node.
+     * @return Shortest Route between source and sink.
+     */
+    public Route shortestPath(GraphNode source, GraphNode sink) {
+        HashMap<GraphNode, GraphNode> prevNode = new HashMap<GraphNode, GraphNode>();
+
+        initializeDistancesToInfinity(source);
+
+        Boolean earlyStop = false;
+        while(!pQueue.isEmpty() && !earlyStop) {
+            PriorityQueueItem current = pQueue.poll();
+
+            // when the sink is polled from the queue, it is at its minimum. The shortest path to it will not
+            // change. So, we can stop early.
+            if (current.getNode() == sink) {
+                earlyStop = true;
+            }
+
+            for (GraphNode neighbor : current.getNode().getNeighbors()) {
+                // removing and reinserting into the pqueue may be needlessly slow?
+                PriorityQueueItem neighborPQueueItem = this.pQueue.remove(neighbor);
+                if (neighborPQueueItem == null) {
+                    // if the neighbor is no longer in the priority queue, its shortest path has already been found.
+                    continue;
+                }
+                Double currentToNeighbor = GraphNode.computeDistance(current.getNode(), neighbor);
+                Double altDistance = current.getDistanceFromSource() + currentToNeighbor;
+                if (altDistance < neighborPQueueItem.getDistanceFromSource()) {
+                    // update the previous node in shortest path to node neighbor.
+                    prevNode.put(neighbor, current.getNode());
+                }
+                // neighbor needs to be replaced in the pqueue whether or not the priority was changed because we
+                // removed it.
+                Double newDist = Math.min(altDistance, neighborPQueueItem.getDistanceFromSource());
+                PriorityQueueItem maybeUpdatedNeighbor = new PriorityQueueItem(neighbor, newDist, newDist);
+                this.pQueue.offer(maybeUpdatedNeighbor);
+            }
+        }
+        this.pQueue.clear();
+        // make the previous node of source be itself. This is the stopping condition for the route construction
+        // function.
+        prevNode.put(source, source);
+        return routeFromIds(source, sink, prevNode);
+    }
+
+    /**
+     * Computes the path with the min. elevation gain from source to sink
+     * That is within the specified maxDistance
+     */
+    public Route minElevationGainPath(GraphNode source, GraphNode sink, double maxDistance){
+        Route route = new Route();
+
+        //TODO
+        return route;
+    }
+
+    /**
+     * Computes the path with the max. elevation gain from source to sink
+     * That is within the specified maxDistance
+     */
+    public Route maxElevationGainPath(GraphNode source, GraphNode sink, double maxDistance){
+        Route route = new Route();
+
+        //TODO
+        return route;
+    }
+
+}
---
+++ src/test/java/com/EleNa/DijkstraRouteFinderTests.java
@@ -0,0 +1,142 @@
+package com.EleNa;
+
+import com.EleNa.graph.Graph;
+import com.EleNa.graph.GraphNode;
+import com.EleNa.routing.DijkstraRouteFinder;
+import com.EleNa.routing.MaxPriorityComparator;
+import com.EleNa.routing.MinPriorityComparator;
+import com.EleNa.routing.Route;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+public class DijkstraRouteFinderTests {
+
+    private GraphNode nodeA;
+    private GraphNode nodeB;
+    private GraphNode nodeC;
+    private GraphNode nodeD;
+    private GraphNode nodeE;
+    private GraphNode nodeF;
+    private GraphNode nodeG;
+
+    private Graph graph;
+
+    private MinPriorityComparator minComp;
+    private MaxPriorityComparator maxComp;
+
+    private DijkstraRouteFinder routeFinder;
+
+    @BeforeEach
+    void startUp() {
+        nodeA  = new GraphNode(0,0.0,0.0,0.0);
+        nodeB = new GraphNode(1,1.0,1.0,10.0);
+        nodeC = new GraphNode(2,0.0,1.0,0.0);
+        nodeD = new GraphNode(3,1.0,0.0,0.0);
+        nodeE = new GraphNode(4,0.0,1.01,1.0);
+        nodeF = new GraphNode(5,0.0,1.02,2.0);
+        nodeG = new GraphNode(6,0.0,1.03,3.0);
+
+
+        nodeA.addNeighbor(nodeB);
+        nodeA.addNeighbor(nodeC);
+        nodeB.addNeighbor(nodeD);
+        nodeB.addNeighbor(nodeG);
+        nodeC.addNeighbor(nodeD);
+        nodeC.addNeighbor(nodeE);
+        nodeE.addNeighbor(nodeF);
+        nodeF.addNeighbor(nodeG);
+
+        graph = new Graph();
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.addNode(nodeD);
+        graph.addNode(nodeE);
+        graph.addNode(nodeF);
+        graph.addNode(nodeG);
+
+        minComp = new MinPriorityComparator();
+        maxComp = new MaxPriorityComparator();
+
+        routeFinder = new DijkstraRouteFinder(graph);
+    }
+
+    @AfterEach
+    void tearDown() {
+        nodeA = null;
+        nodeB = null;
+        nodeC = null;
+        nodeD = null;
+        nodeE = null;
+        nodeF = null;
+        nodeG = null;
+
+        graph = null;
+
+        routeFinder = null;
+    }
+
+    @Test
+    void testConstructor(){
+        assertNotEquals(null, routeFinder);
+    }
+
+    /*@Test
+    void testSetAndGetComparator(){
+        assertEquals(minComp,routeFinder.getComparator());
+
+        routeFinder.setComparator(maxComp);
+
+        assertEquals(maxComp,routeFinder.getComparator());
+    }*/
+
+    @Test
+    void testShortestPath(){
+        Route optimalRoute = routeFinder.shortestPath(nodeA,nodeD);
+
+        assertEquals(3, optimalRoute.getRoute().size());
+        assertEquals(10.0, optimalRoute.getElevationGain());
+        assertEquals(268427.0, Math.floor(optimalRoute.getLength()));
+
+        assertEquals(nodeA,optimalRoute.getRoute().get(0));
+        assertEquals(nodeB,optimalRoute.getRoute().get(1));
+        assertEquals(nodeD,optimalRoute.getRoute().get(2));
+    }
+
+    /**
+     * This test checks that the shortest weighted path is picked rather than a path that uses fewer nodes.
+     */
+    @Test
+    void TestLongerShortestPath() {
+        Route optimalRoute = routeFinder.shortestPath(nodeA,nodeG);
+
+        assertEquals(5, optimalRoute.getRoute().size());
+        assertEquals(3.0, optimalRoute.getElevationGain());
+        assertEquals(nodeA, optimalRoute.getRoute().get(0));
+        assertEquals(nodeC, optimalRoute.getRoute().get(1));
+        assertEquals(nodeE, optimalRoute.getRoute().get(2));
+        assertEquals(nodeF, optimalRoute.getRoute().get(3));
+        assertEquals(nodeG, optimalRoute.getRoute().get(4));
+    }
+
+    /*
+    @Test
+    void testMinElevationGainPath(){
+        Route optimalRoute = routeFinder.shortestPath(nodeA,nodeD);
+
+        Route minElevationRoute = routeFinder.minElevationGainPath(nodeA,nodeD,optimalRoute.getLength() * 1.5);
+
+        assertEquals(3,minElevationRoute.getRoute().size());
+        assertEquals(0.0,minElevationRoute.getElevationGain());
+        assertEquals(268444.0,Math.floor(minElevationRoute.getLength()));
+
+        assertEquals(nodeA,minElevationRoute.getRoute().get(0));
+        assertEquals(nodeC,minElevationRoute.getRoute().get(1));
+        assertEquals(nodeD,minElevationRoute.getRoute().get(2));
+    }*/
+}
