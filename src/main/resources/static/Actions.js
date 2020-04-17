0bf051c2c5f5decd50c4a24eaa7779420b04ca41
Eric Shi <ericshi98@yahoo.com>
2020-04-16 22:32:26 -0400

    Set up html styling and getting input fields.

--- src/main/resources/static/Actions.js
+++ src/main/resources/static/Actions.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-    $('#submitRoute').click(function() {
-        alert('clicked');
-    })
+
+    $('#submit').on('click', function() {
+        let elevation = $('#elevation').val()
+        let withinX = $('#within-x').val()
+        let starting = $('#starting').val()
+        let destination = $('#destination').val()
+        console.log(`elevation:${elevation}\nwithinX:${withinX} \nstarting: ${starting} \ndestination: ${destination}`);
+    });
+
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
 })
