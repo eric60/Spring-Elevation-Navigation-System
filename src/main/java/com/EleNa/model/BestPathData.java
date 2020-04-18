4197b87ebc9e9027ad596148b5c271892e1af1e7
Eric Shi <ericshi98@yahoo.com>
2020-04-18 07:55:32 -0400

    Set up edge passing from BestPathData

--- src/main/java/com/EleNa/model/BestPathData.java
+++ src/main/java/com/EleNa/model/BestPathData.java
@@ -1,8 +1,6 @@
 package com.EleNa.model;
 
 import com.EleNa.routing.RouteFinder;
-
-import java.text.Normalizer;
 import java.util.ArrayList;
 
 public class BestPathData {
@@ -11,12 +9,24 @@ public class BestPathData {
 
     public double[][] calculateRoute(FormData form) {
         // all edge pairs , [n1,n2] [n2,n4]
-        int edgesCnt = 3;
+        int edgesCnt = 11;
         int edgePair = 2;
-        double[][] arr = new double[edgesCnt][edgePair];
-        arr[0][0] = 72.33332;
-        arr[0][1] = 22.22222;
-        return arr;
+
+        double[][] edges = new double[edgesCnt][edgePair];
+        edges = new double[][]{
+                {-77.0366048812866, 38.89873175227713},
+                {-77.03364372253417, 38.89876515143842},
+                {-77.03364372253417, 38.89549195896866},
+                {-77.02982425689697, 38.89549195896866},
+                {-77.02400922775269, 38.89387200688839},
+                {-77.01519012451172, 38.891416957534204},
+                {-77.01521158218382, 38.892068305429156},
+                {-77.00813055038452, 38.892051604275686},
+                {-77.00832366943358, 38.89143365883688},
+                {-77.00818419456482, 38.89082405874451},
+                {-77.00815200805664, 38.88989712255097}
+        };
+        return edges;
     }
 
 
--- src/main/resources/static/Actions.js
+++ src/main/resources/static/Actions.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     let elevation, withinX, starting, destination;
+
     $('#submit').on('click', function() {
         elevation = $('#elevation').val()
         withinX = $('#within-x').val()
@@ -8,7 +9,6 @@ $(document).ready(function() {
         console.log(`elevationPref:${elevation}\nwithinX:${withinX} \nstarting: ${starting} \ndestination: ${destination}`);
         postToSpring();
 
-        initMapRoute();
     });
 
     function postToSpring() {
@@ -24,7 +24,9 @@ $(document).ready(function() {
             data: JSON.stringify(elenaData)
         })
         .done(function( msg ) {
-            console.log(msg);
+             let edges = msg;
+             console.log(edges)
+             initMapRoute(edges)
         });
     }
 
@@ -44,19 +46,7 @@ $(document).ready(function() {
                 'geometry': {
                     'type': 'LineString',
                     'properties': {},
-                    'coordinates': [
-                        [-77.0366048812866, 38.89873175227713],
-                        [-77.03364372253417, 38.89876515143842],
-                        [-77.03364372253417, 38.89549195896866],
-                        [-77.02982425689697, 38.89549195896866],
-                        [-77.02400922775269, 38.89387200688839],
-                        [-77.01519012451172, 38.891416957534204],
-                        [-77.01521158218382, 38.892068305429156],
-                        [-77.00813055038452, 38.892051604275686],
-                        [-77.00832366943358, 38.89143365883688],
-                        [-77.00818419456482, 38.89082405874451],
-                        [-77.00815200805664, 38.88989712255097]
-                    ]
+                    'coordinates': []
                 }
             }
         ]
@@ -74,7 +64,8 @@ $(document).ready(function() {
         mapboxgl: mapboxgl
     });
 
-    function initMapRoute() {
+    function initMapRoute(edges) {
+            geojson.features[0].geometry.coordinates = edges;
 
             map.addSource('LineString', {
                 'type': 'geojson',
