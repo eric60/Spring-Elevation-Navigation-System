1118528d712c220ea8679971a1cf3caba5a2b4cf
Eric Shi <ericshi98@yahoo.com>
2020-04-18 21:31:24 -0400

    Added start and end popups to show address.

--- src/main/resources/static/Actions.js
+++ src/main/resources/static/Actions.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
     let elevation, withinX, starting, destination;
+    let test = [
+        [-77.0366048812866, 38.89873175227713],
+        [-77.03364372253417, 38.89876515143842],
+        [-77.03364372253417, 38.89549195896866],
+        [-77.02982425689697, 38.89549195896866],
+        [-77.02400922775269, 38.89387200688839],
+        [-77.01519012451172, 38.891416957534204],
+        [-77.01521158218382, 38.892068305429156],
+        [-77.00813055038452, 38.892051604275686],
+        [-77.00832366943358, 38.89143365883688],
+        [-77.00818419456482, 38.89082405874451],
+        [-77.00815200805664, 38.88989712255097]
+    ]
 
     $('#submit').on('click', function() {
         elevation = $('#elevation').val()
@@ -13,7 +26,6 @@ $(document).ready(function() {
     });
 
     function getCoordinates(addresses) {
-        let startCoord = [], endCoord = [];
         let promises = []
 
         for (let i = 0; i < addresses.length; i++) {
@@ -92,6 +104,7 @@ $(document).ready(function() {
         .done(function( edges ) {
              console.log('------- Received edges --------')
              console.log(edges)
+
              initMapRoute(edges)
         });
     }
@@ -151,7 +164,20 @@ $(document).ready(function() {
         console.log('---- in init map')
         console.log(start);
         console.log(end)
+        zoomToLine();
         addStartandStopPoints(start, end)
+    }
+
+    function zoomToLine() {
+        var coordinates = geojson.features[0].geometry.coordinates;
+
+        var bounds = coordinates.reduce(function(bounds, coord) {
+            return bounds.extend(coord);
+        }, new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]));
+
+        map.fitBounds(bounds, {
+            padding: 20
+        });
     }
 
     function addStartandStopPoints(start, end) {
@@ -167,7 +193,7 @@ $(document).ready(function() {
                             'coordinates': start
                         },
                         'properties': {
-                            'title': starting
+                            'title': `<strong>${starting}</strong>`
                         }
                     },
                     {
@@ -186,6 +212,8 @@ $(document).ready(function() {
         });
         addPointColor("start", start)
         addPointColor("end", end)
+        addPopup("start")
+        addPopup("end")
     }
 
     function addPointColor(type, coords) {
@@ -216,34 +244,39 @@ $(document).ready(function() {
         });
     }
 
+    function addPopup(type) {
+        map.on('click', type, function(e) {
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            let title;
+            if (type == "start") {
+                title = `<b>${starting}</b>`
+            } else {
+                title = `<b>${destination}</b>`
+            }
+           // var title = e.features[0].properties.title;
 
-    // When a click event occurs on a feature in the places layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'points', function(e) {
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var title = e.features[0].properties.title;
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
 
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(title)
+                .addTo(map);
+        });
 
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(title)
-            .addTo(map);
-    });
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', type, function() {
+            map.getCanvas().style.cursor = 'pointer';
+        });
 
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'places', function() {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'places', function() {
-        map.getCanvas().style.cursor = '';
-    });
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', type, function() {
+            map.getCanvas().style.cursor = '';
+        });
+    }
 
 })
