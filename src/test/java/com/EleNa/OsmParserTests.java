fa2842c017992159c6862686e5e5269158063a5c
Eric Shi <ericshi98@yahoo.com>
2020-04-27 19:11:33 -0400

    Fixed OSM Parser tests.

--- src/test/java/com/EleNa/OsmParserTests.java
+++ src/test/java/com/EleNa/OsmParserTests.java
@@ -13,6 +13,10 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import org.springframework.test.context.ActiveProfiles;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -36,12 +40,12 @@ public class OsmParserTests {
     @Test
     public void test_given_osm_when_importData_then_Correct_number_of_nodes_and_ways() {
         // 1) Make sure node that is not in a way edge path is not saved
-        String windowsFile = "C:\\Users\\T450-180519\\Documents\\Coding_Projects\\520-Project\\src\\main\\resources\\map_small_test.osm";
-        String macFile = "/Users/JohnBurns/Desktop/CS520/Elena/520-Project/src/main/resources/map_small_test.osm";
+        String file = "./src/main/resources/map_small_test.osm";
+
         Mockito.when(nodeRepo.saveAll(null)).thenReturn(null);
         Mockito.when(edgeRepo.saveAll(null)).thenReturn(null);
         importer = new OsmParser(nodeRepo, edgeRepo);
-        importer.importData(windowsFile);
+        importer.importData(file);
         assertEquals(46, importer.nodeCnt);
         assertEquals(3, importer.wayCnt);
         assertEquals(45, importer.edgeCnt);
